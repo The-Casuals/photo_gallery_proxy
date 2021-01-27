f026173516ff5ce488cb5dd3d7aa17ce6fafb3f0
Jacky Xia <jacky.w.xia@gmail.com>
2021-01-27 00:15:04 -0800

    Update api get route to 8080

--- server/api.js
+++ server/api.js
@@ -4,25 +4,25 @@ const axios = require('axios');
 const api = express.Router();
 
 api.get('/galleries/:id', (req, res) => {
-  axios.get(`http://72.134.126.46/api/galleries/${req.params.id}`)
+  axios.get(`http://72.134.126.46:8080/api/galleries/${req.params.id}`)
     .then(({data}) => res.status(200).send(data))
     .catch((err) => res.status(400)/send(err));
 });
 
 api.get('/checkout/:id', (req, res) => {
-  axios.get(`http://34.219.249.230/api/checkout/${req.params.id}`)
+  axios.get(`http://34.219.249.230:8080/api/checkout/${req.params.id}`)
     .then(({data}) => res.status(200).send(data))
     .catch((err) => res.status(400).send(err));
 })
 
 api.get('/img_carousel/:id', (req, res) => {
-  axios.get(`http://3.101.61.202/api/img_carousel/${req.params.id}`)
+  axios.get(`http://3.101.61.202:8080/api/img_carousel/${req.params.id}`)
     .then(({data}) => res.status(200).send(data))
     .catch((err) => res.status(400).send(err));
 })
 
 api.get('/reviews/:id', (req, res) => {
-  axios.get(`http://54.215.215.126/api/reviews/${req.params.id}`)
+  axios.get(`http://54.215.215.126:8080/api/reviews/${req.params.id}`)
     .then(({data}) => res.status(200).send(data))
     .catch((err) => res.status(400).send(err));
 })
