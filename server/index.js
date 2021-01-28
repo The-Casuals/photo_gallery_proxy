b8f8db35b496a344807502dadabb81f7cd4ec0ff
Jacky Xia <jacky.w.xia@gmail.com>
2021-01-28 11:18:44 -0800

    Optimize with compression and update IP address for brandon

--- server/index.js
+++ server/index.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const compression = require('compression');
 
 const app = express();
-// const bundles = require('./bundles');
 const api = require('./api');
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(compression());
 app.use('/:id', express.static(path.join(__dirname, '..', 'public')));
 
-// app.use('/:id/bundles', bundles);
 app.use('/api', api);
 
 const PORT = 3000;
